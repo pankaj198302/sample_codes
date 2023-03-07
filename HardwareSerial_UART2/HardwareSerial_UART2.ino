1eff10fe9e1a3b7a4b4cf759e93ad228cb620b21
pankaj198302 <67203272+pankaj198302@users.noreply.github.com>
2023-03-07 13:33:47 +0530

    Tested NINA-W1 Module UART1 and UART2

--- HardwareSerial_UART2/HardwareSerial_UART2.ino
+++ HardwareSerial_UART2/HardwareSerial_UART2.ino
@@ -1,11 +1,22 @@
- #define RXD2 16
- #define TXD2 17
  // This sample code uses GPIO 16 and GPIO17 for RXD2 and TXD2. ON ESP32 Wroom module the pins are named as RX2 and TX2
+ //#define RXD2 16
+ //#define TXD2 17
+// Here We are testing with NINA-W106 -- Doesn't work
+ //#define RXD2 13
+// #define TXD2 12
+
+ // Here We are testing with NINA-W106  -- Refer NINA-EVK USer Guide Working NINA-W106 MOP 16 and MOP 17 Working
+ //#define RXD2 26
+ //#define TXD2 25
+  // Here We are testing with NINA-W106  -- Refer NINA-EVK USer Guide Working NINA-W106 MOP 20 and MOP 21 Working
+ //#define RXD2 22
+ //#define TXD2 19
+ 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   Serial2.begin(115200, SERIAL_8N1, RXD2, TXD2);
-  delay(1000);
+  delay(1000*5);
   Serial.println("Loopback program started");
 
 }
